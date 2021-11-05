3b56faae585349f598369f39bf0f49cb62576892
unknown <Martin.A.D.Harvey@hotmail.co.uk>
2021-11-05 04:17:11 +0000

    Added functionality for string based enums

--- JSONCardLoader.cs
+++ JSONCardLoader.cs
@@ -8,9 +8,9 @@ using DiskCardGame;
 using HarmonyLib;
 
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 using UnityEngine;
 
@@ -20,14 +20,30 @@ namespace JSONCardParserPlugin
 	[BepInDependency( "cyantist.inscryption.api", BepInDependency.DependencyFlags.HardDependency )]
 	public class Plugin : BaseUnityPlugin
 	{
-		private const string PluginGuid = "MADH.inscryption.JSONCardParser";
-		private const string PluginName = "JSONCardParser";
-		private const string PluginVersion = "1.0.0.0";
+
+		public bool getTestDeck()
+		{
+			return Config.Bind( "JSONCardLoader", "TestDeck", false, new BepInEx.Configuration.ConfigDescription( "Load start deck with specified cards" ) ).Value;
+		}
+
+		public List<string> getCards()
+		{
+			List<string> list = new List<string>(4);
+			list.Add( Config.Bind( "JSONCardLoader", "Card1", "Wolf" ).Value );
+			list.Add( Config.Bind( "JSONCardLoader", "Card2", "Opossum" ).Value );
+			list.Add( Config.Bind( "JSONCardLoader", "Card3", "Stoat_Talking" ).Value );
+			list.Add( Config.Bind( "JSONCardLoader", "Card4", "Bullfrog" ).Value );
+			return list;
+		}
+
+		private const string PluginGuid = "MADH.inscryption.JSONCardLoader";
+		private const string PluginName = "JSONCardLoader";
+		private const string PluginVersion = "1.1.0.0";
 
 		internal static ManualLogSource Log;
 
-		static readonly string[] artPaths = { Paths.PluginPath, "CardLoader", "Artwork" };
-		static readonly string[] jsonPaths = { Paths.PluginPath, "CardLoader", "Cards" };
+		static readonly string[] artPaths = { Paths.PluginPath, "JSONLoader", "Artwork" };
+		static readonly string[] jsonPaths = { Paths.PluginPath, "JSONLoader", "Cards" };
 
 		public static readonly string ArtPath = Path.Combine(artPaths);
 		private static readonly string JSONPath = Path.Combine(jsonPaths);
@@ -40,6 +56,9 @@ namespace JSONCardParserPlugin
 			Harmony harmony = new Harmony( PluginGuid );
 			harmony.PatchAll();
 
+			//Load Config
+			getTestDeck();
+			getCards();
 
 			Logger.LogInfo( "Loading cards from JSON..." );
 
@@ -53,6 +72,68 @@ namespace JSONCardParserPlugin
 			}
 		}
 	}
+
+	public struct Dicts
+	{
+		public static Dictionary<string, int> Priority = new Dictionary<string, int>()
+		{
+			{ "High",	0 },
+			{ "Medium", 1 },
+			{ "Low",	2 }
+		};
+
+		public static Dictionary<string, CardMetaCategory> MetaCategory
+			=	Enum.GetValues( typeof( CardMetaCategory ) )
+					.Cast< CardMetaCategory >()
+					.ToDictionary( t => t.ToString(), t => t );
+
+		public static Dictionary<string, CardComplexity> Complexity 
+			=	Enum.GetValues( typeof( CardComplexity ) )
+					.Cast< CardComplexity >()
+					.ToDictionary( t => t.ToString(), t => t );
+
+		public static Dictionary<string, CardTemple> Temple 
+			=	Enum.GetValues( typeof( CardTemple ) )
+					.Cast< CardTemple >()
+					.ToDictionary( t => t.ToString(), t => t );
+
+		public static Dictionary<string, GemType> GemColour 
+			=   Enum.GetValues( typeof( GemType ) )
+					.Cast< GemType >()
+					.ToDictionary( t => t.ToString(), t => t );
+
+		public static Dictionary<string, SpecialStatIcon> StatIcon 
+			=   Enum.GetValues( typeof( SpecialStatIcon ) )
+					.Cast< SpecialStatIcon >()
+					.ToDictionary( t => t.ToString(), t => t );
+
+		public static Dictionary<string, Tribe> Tribes 
+			=   Enum.GetValues( typeof( Tribe ) )
+					.Cast< Tribe >()
+					.ToDictionary( t => t.ToString(), t => t );
+
+		public static Dictionary<string, Trait> Traits 
+			=   Enum.GetValues( typeof( Trait ) )
+					.Cast< Trait >()
+					.ToDictionary( t => t.ToString(), t => t );
+
+		public static Dictionary<string, SpecialTriggeredAbility> SpecialAbilities 
+			=   Enum.GetValues( typeof( SpecialTriggeredAbility ) )
+					.Cast< SpecialTriggeredAbility >()
+					.ToDictionary( t => t.ToString(), t => t );
+
+		public static Dictionary<string, Ability> Abilities 
+			=   Enum.GetValues( typeof( Ability ) )
+					.Cast< Ability >()
+					.ToDictionary( t => t.ToString(), t => t );
+
+		public static Dictionary<string, CardAppearanceBehaviour.Appearance> AppearanceBehaviour 
+			=   Enum.GetValues( typeof( CardAppearanceBehaviour.Appearance ) )
+					.Cast< CardAppearanceBehaviour.Appearance >()
+					.ToDictionary( t => t.ToString(), t => t );
+	}
+
+
 
 	[System.Serializable]
 	public class EvolveData
@@ -77,28 +158,28 @@ namespace JSONCardParserPlugin
 	[System.Serializable]
 	public class CardData
 	{
-		public int priority;
+		public string priority;
 		public string name, displayedName, description;
-		public List<int> metaCategories;
-		public int cardComplexity;
-		public int temple;
+		public List<string> metaCategories;
+		public string cardComplexity;
+		public string temple;
 		public int baseAttack;
 		public int baseHealth;
 		public bool hideAttackAndHealth;
 		public int cost, bonesCost, energyCost;
-		public List<int> gemsCost;
-		public int specialStatIcon;
-		public List<int> tribes;
-		public List<int> traits;
-		public List<int> specialAbilities;
-		public List<int> abilities;
+		public List<string> gemsColour;
+		public string specialStatIcon;
+		public List<string> tribes;
+		public List<string> traits;
+		public List<string> specialAbilities;
+		public List<string> abilities;
 		public EvolveData evolveParams;
 		public string defaultEvolutionName;
 		public TailData tailParams;
 		public IceCubeData iceCubeParams;
 		public bool flipPortraitForStrafe;
 		public bool onePerDeck;
-		public List<int> appearanceBehaviour;
+		public List<string> appearanceBehaviour;
 		public string texture, altTexture;
 		public string titleGraphic;
 		public string pixelTexture;
@@ -110,52 +191,51 @@ namespace JSONCardParserPlugin
 			return JsonUtility.FromJson<CardData>( jsonString );
 		}
 
-		public static Texture2D LoadTexture2D( string image )
+		public static Texture2D LoadTexture2D( (int x, int y) size, string image )
 		{
-			return image == null ? null : new Texture2D( 2, 2 ).WithImage( image );
+			return image == null ? null : new Texture2D( size.x, size.y ).WithImage( image );
 		}
 
 		public static void GenerateNewCard( CardData card )
 		{
 			new NewCard(
 				card.name,
-				card.metaCategories.AsEnum<CardMetaCategory>(),
-				(CardComplexity)card.cardComplexity,
-				(CardTemple)card.temple,
+				card.metaCategories.AsEnum( Dicts.MetaCategory ),
+				card.cardComplexity == null ? CardComplexity.Vanilla : Dicts.Complexity[ card.cardComplexity ],
+				card.temple == null ? CardTemple.Nature : Dicts.Temple[ card.temple ],
 				card.displayedName ?? "",
 				card.baseAttack, card.baseHealth == 0 ? 1 : card.baseHealth,
 				card.description ?? "",
 				card.hideAttackAndHealth,
-				card.cost, card.bonesCost,
-				card.energyCost,
-				card.gemsCost.AsEnum<GemType>(),
-				(SpecialStatIcon)card.specialStatIcon,
-				card.tribes.AsEnum<Tribe>(),
-				card.traits.AsEnum<Trait>(),
-				card.specialAbilities.AsEnum<SpecialTriggeredAbility>(),
-				card.abilities.AsEnum<Ability>(),
-				null, //TODO
+				card.cost, card.bonesCost, card.energyCost,
+				card.gemsColour?.AsEnum( Dicts.GemColour ),
+				card.specialStatIcon == null ? SpecialStatIcon.None : Dicts.StatIcon[ card.specialStatIcon ],
+				card.tribes?.AsEnum( Dicts.Tribes ),
+				card.traits?.AsEnum( Dicts.Traits ),
+				card.specialAbilities?.AsEnum( Dicts.SpecialAbilities ),
+				card.abilities?.AsEnum( Dicts.Abilities ),
+				null, //TODO: implement
 				card.defaultEvolutionName,
-				null, //TODO
-				null, //TODO
+				null, //TODO: implement
+				null, //TODO: implement
 				card.flipPortraitForStrafe,
 				card.onePerDeck,
-				card.appearanceBehaviour.AsEnum<CardAppearanceBehaviour.Appearance>(),
-				CardData.LoadTexture2D( card.texture ),
-				CardData.LoadTexture2D( card.altTexture ),
-				CardData.LoadTexture2D( card.titleGraphic ),
-				CardData.LoadTexture2D( card.pixelTexture ),
-				null, //TODO
-				card.decals.AsTextures()
+				card.appearanceBehaviour?.AsEnum( Dicts.AppearanceBehaviour ),
+				CardData.LoadTexture2D( (2, 2), card.texture ),
+				CardData.LoadTexture2D( (2, 2), card.altTexture ), //TODO: fix sizes
+				CardData.LoadTexture2D( (2, 2), card.titleGraphic ),
+				CardData.LoadTexture2D( (2, 2), card.pixelTexture ),
+				null, //TODO: implement
+				card.decals?.AsTextures()
 			);
 		}
 	}
 
 	public static class UtilityExtensions
 	{
-		public static List<T> AsEnum<T>( this List<int> list ) where T : struct, IConvertible
+		public static List<T> AsEnum<T>( this List<string> list, Dictionary<string, T> dict ) where T : struct, IConvertible
 		{
-			return list.Select( i => (T)(object)i ).ToList();
+			return list.Select( i => dict[i] ).ToList();
 		}
 
 		public static List<Texture> AsTextures( this List<string> list )
@@ -175,6 +255,25 @@ namespace JSONCardParserPlugin
 			byte[] imgBytes = File.ReadAllBytes( Path.Combine( Plugin.ArtPath, image ) );
 			texture.LoadImage( imgBytes );
 			return texture;
+		}
+	}
+
+	[HarmonyPatch( typeof( DeckInfo ), "InitializeAsPlayerDeck" )]
+	public class DeckInfo_InitializeAsPlayerDeck : DeckInfo
+	{
+		[HarmonyPrefix]
+		public static bool Prefix( ref DeckInfo __instance )
+		{
+			Plugin p = new Plugin();
+			if ( !p.getTestDeck() ) return true;
+
+			List<string> Cards = p.getCards();
+			__instance.AddCard( CardLoader.GetCardByName( Cards[0] ) );
+			__instance.AddCard( CardLoader.GetCardByName( Cards[1] ) );
+			__instance.AddCard( CardLoader.GetCardByName( Cards[2] ) );
+			__instance.AddCard( CardLoader.GetCardByName( Cards[3] ) );
+
+			return false;
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # JSON Card Loader
 
 This is a BepInEx plugin made for Incryption to create custom cards using JSON files and the API by Cyantist.
-It can currently parse custom cards and pass them to APIPlugin to load them into the game. Cards with custom EvolveParams, TailParams, and IceCubeParams are not yet supported.
+It can currently parse custom cards and pass them to APIPlugin to load them into the game. Cards with custom EvolveParams, TailParams, IceCubeParams, and animatedPortraits are not yet supported.
 
 ## Installation
 ###Requirements
@@ -13,46 +13,46 @@ To install this mod, you simply need to put the **JSONCardLoaderPlugin.dll** in 
 
 ## Custom Cards
 
-To create your own cards you just create a .json file and fill in all the parameters you want your card to have (parameters you don't include will be defaulted). An example file is included in this repo.
+To create your own cards you just create a .json file and fill in all the parameters you want your card to have (parameters you don't include will be defaulted). Some parameters are required, and some are optional with default values. Those parameters and their values are specified in the table below. For reference, an example file is included in this repo.
 
 All custom cards must have their json files be in a **Cards** folder, and their art in an **Artwork** folder in the same path as the dll.
 
-Cards have lots of fields that can be edited - this is a list of all fields names and what they should include. The names must be exactly as written below into the json file.
+Cards have lots of fields that can be edited - this is a list of all fields names and their purpose. The parameters you wish to include in the json file should be copied exactly from this table, and any parameters that refer to *Enum.txt* should have their strings be copied exactly from there.
 
 | Parameter | Description |
 |------|-------------|
-| priority | Currently Unused |
-| name | The name the game will use to identify the card - should contain no spaces |
-| displayedName | The name displayed on the card |
-| discription | The description Leshy gives when you find the card |
-| metaCategories | An integer array of meta catagories the card has (See enums.txt for which values relate to what category) |
-| cardComplexity | An integer value for the complexity of the card (See enums.txt for which values relate to which level of complexity) |
-| temple | An integer value for which Scrybe created the card |
-| baseAttack | An integer value for the attack of a card |
-| baseHealth | An integer value for the health of a card |
-| hideAttackAndHealth | A boolean value to toggle if the cards attack and health are visible |
-| cost | An integer value for the blood cost of a card |
-| bonesCost | An integer value for the bones cost of a card |
-| energyCost | An integer value for the energy cost of a card |
-| gemsCost | An integer array for the gems cost of a card (See enums.txt for what values relate to which gem) |
-| specialStatIcon | An integer value for which special stat icon the card has (See enums.txt for which value relates to which icon) |
-| tribes | An integer array for the tribes the card belongs to (See enums.txt for which values relate to which tribe) |
-| traits | An integer array for the traits a card has (See enums.txt for which values relate to which trait) |
-| specialAbilities | An integer array for the special abilities a card has (See enums.txt for which values relate to which special ability) |
-| abilities | An integer array for the sigils a card has. (See enums.txt for which values relate to which sigil ability).
-| evolveParams | Currently Unavailable |
-| defaultEvolutionName | The default name the evolution will take |
-| tailParams | Currently Unavailable |
-| iceCubeParams | Currently Unavailable |
-| flipPortraitForStrafe | A boolean to determine if the cards portrait should flip when it uses one of the strafe sigils |
-| onePerDeck | A boolean value that toggles if there can be only one of the card per deck |
-| appearanceBehaviour | An integer array for the behaviours the cards appearance should have (See enums.txt for which values relate to which appearance behaviours) |
-| texture | A string for the image name. If it is in a subfolder within **Artwork** the subfolder can proceed the file name seperated by a '/' |
-| altTexture | A string for cards alternate image name |
-| titleGraphic | A string for the cards title image name (like Tentacle cards in act1) |
-| pixelTexture | A string for the cards act2 image name |
-| animatedPortrait | Currently Unavailable |
-| decals | A string array for the texture names of a card decals | 
+| priority | **[Unused]** |
+| name | **[Required]** A string for the name the game will use to identify the card - should contain no spaces |
+| displayedName | **[Required]** A string for the name displayed on the card |
+| discription | **[Required]** A string for the description Leshy gives when you find the card |
+| metaCategories | **[Required]** A string array of meta catagories the card has (See *Enums.txt* for a list of catagories) |
+| cardComplexity | **[Optional]** **[Default: Vanilla]** A string for the complexity of the card (See *Enums.txt* for a list of levels of complexity) |
+| temple | **[Optional]** **[Default: Nature]** A string for which Scrybe created the card |
+| baseAttack | **[Optional]** **[Default: 0]** An integer value for the attack of a card |
+| baseHealth | **[Optional]** **[Default: 1]** An integer value for the health of a card |
+| hideAttackAndHealth | **[Default: false]** A boolean value to toggle if the cards attack and health are visible |
+| cost | **[Optional]** **[Default: 0]** An integer value for the blood cost of a card |
+| bonesCost | **[Optional]** An integer value for the bones cost of a card |
+| energyCost | **[Optional]** An integer value for the energy cost of a card |
+| gemsCost | **[Optional]** A string array for the gems cost of a card (See *Enums.txt* for a list of gems) |
+| specialStatIcon | **[Optional]** An string for which special stat icon the card has (See *Enums.txt* for a list of icons) |
+| tribes | **[Optional]** An string array for the tribes the card belongs to (See *Enums.txt* for a list of tribes) |
+| traits | **[Optional]** An string array for the traits a card has (See *Enums.txt* for a list of traits) |
+| specialAbilities | **[Optional]** A string array for the special abilities a card has (See *Enums.txt* for a list of special abilities) |
+| abilities | **[Optional]** A string array for the sigils a card has. (See *Enums.txt* for a list of sigil abilities).
+| evolveParams | **[Unavailable]** |
+| defaultEvolutionName | **[Optional]** The default name the evolution will have |
+| tailParams | **[Unavailable]** |
+| iceCubeParams | **[Unavailable]** |
+| flipPortraitForStrafe | **[Optional]** A boolean to determine if the cards portrait should flip when it uses one of the strafe sigils |
+| onePerDeck | **[Optional]** A boolean value that toggles if there can be only one of the card per deck |
+| appearanceBehaviour | **[Optional]** A string array for the behaviours the cards appearance should have (See enums.txt for a list of appearance behaviours) |
+| texture | **[Optional]** A string for the name of the card's image (including extension). If it is in a subfolder within *Artwork* the subfolder can preceed the file name seperated by a '/' (or your system equivelent) |
+| altTexture | **[Optional]** A string the name of the card's alternate image (including extension) |
+| titleGraphic | **[Optional]** A string for the name of the card's title image (including extension) |
+| pixelTexture | **[Optional]** A string for the name of the card's act2 image (including extension) |
+| animatedPortrait | **[Unavailable]** |
+| decals | **[Optional]** A string array for the texture names of a card decals | 
 
 ## Debugging
 The easiest way to check if the plugin is working properly or to debug an error is to enable the console. This can be done by changing
@@ -72,13 +72,31 @@ to
 Enabled = true
 ```
 in **Inscryption/BepInEx/Config/BepInEx.cfg**
+
 ___
 
-If you want help debugging you can find me on [Inscryption modding discord](https://discord.gg/QrJEF5Denm) as Cyantist.
+You can change **MADH95.inscryption.JSONLoader.cfg** (same location as above) to load up to 4 of your custom cards in the starting deck of a new run. You simply have to enable it by changing
+```
+## Load start deck with specified cards
+# Setting type: Boolean
+# Default value: false
+TestDeck = false
+```
+to
+```
+## Load start deck with specified cards
+# Setting type: Boolean
+# Default value: false
+TestDeck = true
+```
+then change the card names to the *name* parameter you set in the json file. The game should load a chapter 2 (one after the tutorial) run with those cards in the starting deck!
+
+___
+
+If you want help debugging you can find me on [Inscryption modding discord](https://discord.gg/QrJEF5Denm) as MADH95.
 
 ## Development
 
 Plans for the future:
+ - Add functionality for EvolveParams, TailParams, IceCubeParams and animatedPortraits
  - Add functionality for updating existing cards
- - Add functionality for EvolveParams, TailParams, and IceCubeParams
- - Figure out how to Parse strings to enums for better usability
