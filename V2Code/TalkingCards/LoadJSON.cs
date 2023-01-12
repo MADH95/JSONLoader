3c57e867b3031b70a37d3d61c5035a43a1dde612
KBMackenzie <109933089+KBMackenzie@users.noreply.github.com>
2023-01-12 20:20:36 -0300

    Added talking cards

---
+++ V2Code/TalkingCards/LoadJSON.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using BepInEx;
+using Newtonsoft.Json;
+using HarmonyLib;
+using InscryptionAPI.TalkingCards.Create;
+using InscryptionAPI.TalkingCards.Animation;
+using InscryptionAPI;
+using JLPlugin;
+using InscryptionAPI.TalkingCards;
+
+#nullable enable
+namespace JSONLoader.TalkingCards.JSON
+{
+    internal static class LoadJSON
+    {
+        private static List<string> GetTalkingJSON()
+            => Directory.GetFiles(Paths.PluginPath, "*_talk.jldr2", SearchOption.AllDirectories).ToList();
+
+        public static void LoadJSONCards()
+            => GetTalkingJSON().ForEach(x => LoadTalkJSON(x));
+
+        private static void LogInfo(string message)
+            => Plugin.Log.LogInfo(message);
+
+        private static void LoadTalkJSON(string file)
+        {
+            LogInfo($"Loading file: {Path.GetFileName(file)}");
+
+            try
+            {
+                TalkingJSONData? talk = JsonConvert.DeserializeObject<TalkingJSONData>(File.ReadAllText(file));
+                if (talk == null) return;
+                //FileLog.Log($"Loading card: {talk.cardName}");
+                TalkingCardManager.Create(talk.GetFaceData(), GeneratePortrait.DialogueDummy);
+                var dialogueEvents = talk.MakeDialogueEvents();
+                dialogueEvents.ForEach(x => TalkingCardManager.AddToDialogueCache(x?.id));
+            }
+            catch (Exception)
+            {
+                Plugin.Log.LogError($"Error loading JSON data from file {Path.GetFileName(file)}!");
+                // Plugin.LogError(ex.ToString());
+                // throw;
+            }
+        }
+    }
+}
---
+++ V2Code/TalkingCards/RenameFiles.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace JSONLoader.V2Code.TalkingCards
+{
+    internal static class RenameFiles
+    {
+        internal static List<string> FindJSON()
+            => Directory.GetFiles(Paths.PluginPath, "*_talk.json", SearchOption.AllDirectories)
+            .ToList();
+
+        internal static void RenameAll()
+            => FindJSON().ForEach(Rename);
+
+        internal static void Rename(string fileName)
+        {
+            if (!fileName.EndsWith("_talk.json")) return;
+            string name = Path.GetFileNameWithoutExtension(fileName);
+            File.Move(fileName, $"{name}.jldr2");
+        }
+    }
+}
