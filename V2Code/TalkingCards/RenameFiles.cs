622b75c4e7161e4443cefdc74b0897802e603019
KBMackenzie <109933089+KBMackenzie@users.noreply.github.com>
2023-01-13 06:35:48 -0300

    Clean up

--- V2Code/TalkingCards/RenameFiles.cs
+++ V2Code/TalkingCards/RenameFiles.cs
@@ -14,12 +14,19 @@ namespace JSONLoader.Data.TalkingCards
         internal static void RenameAll()
             => FindJSON().ForEach(Rename);
 
+        private static void LogInfo(string message)
+            => JLPlugin.Plugin.Log.LogInfo(message);
+
         internal static void Rename(string fileName)
         {
             if (!fileName.EndsWith("_talk.json")) return;
             string noExtension = fileName.Substring(0, fileName.Length - ".json".Length);
             string newFileName = $"{noExtension}.jldr2";
-            if (!File.Exists(newFileName)) File.Move(fileName, newFileName);
+            if (!File.Exists(newFileName))
+            {
+                File.Move(fileName, newFileName);
+                LogInfo($"Renamed file \'{Path.GetFileName(fileName)}\' to \'{Path.GetFileName(newFileName)}\' for backwards compatibility.");
+            }
         }
     }
 }
