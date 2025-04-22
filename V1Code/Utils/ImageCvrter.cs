d9b9f3bded74b04fdfbc12563de28ca353c62526
Thin Creator 3483 <102002463+SaxbyMod@users.noreply.github.com>
2025-04-22 15:58:59 -0400

    Progress thus far
    Kinda forgot about this but figured id send over the commit

---
+++ V1Code/Utils/ImageCvrter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using System.IO;
+
+namespace JSONLoader.V1Code.Utils
+{
+        public static class ImageLoader
+        {
+            // Load a custom texture from the specified path
+            public static Texture2D LoadCustomTexture(string fileName, string imagePath)
+            {
+                string imageToLoad = Path.Combine(imagePath, fileName + ".png");
+                if (File.Exists(imageToLoad))
+                {
+                    byte[] data = File.ReadAllBytes(imageToLoad);
+                    Texture2D texture2D = new Texture2D(2, 2, TextureFormat.RGBA32, false); // Use RGBA32 format for better color representation
+                    texture2D.LoadImage(data);
+                    return texture2D;
+                }
+                Debug.LogWarning($"Texture not found at path: {imageToLoad}");
+                return null;
+            }
+
+            // Load a custom PNG texture from the specified path
+            public static Texture2D LoadCustomPNG(string fileName, string imagePath)
+            {
+                return LoadCustomTexture(fileName, imagePath); // Reuse LoadCustomTexture for PNG files
+            }
+
+            // Create a Sprite from a custom image file
+            public static Sprite GetCustomImage(string fileName, string imagePath)
+            {
+                Texture2D texture2D = LoadCustomPNG(fileName, imagePath);
+                if (texture2D != null)
+                {
+                    Sprite sprite = Sprite.Create(texture2D, new Rect(0f, 0f, texture2D.width, texture2D.height), Vector2.zero);
+                    sprite.name = fileName; // Name the sprite for easy identification
+                    return sprite;
+                }
+                Debug.LogWarning($"Sprite creation failed for: {fileName} at path: {imagePath}");
+                return null;
+            }
+        }
+}
--- V2Code/AppearenceBehaviors.cs
+++ V2Code/AppearenceBehaviors.cs
@@ -1,13 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using UnityEngine;
 using TinyJson;
+using System.Linq;
+using Sirenix.Utilities;
+using JSONLoader.V1Code.Utils;
+using DiskCardGame;
+using InscryptionAPI.Helpers;
 
 namespace JLPlugin.Data
 {
     [System.Serializable]
     public class AppearenceBehaviors
     {
+        public class AppearenceBackgroundInfo
+        {
+            public Sprite CardBack {get; set;}
+        }
+
+        public class AppearenceCardBackground(AppearenceBackgroundInfo info) : CardAppearanceBehaviour
+        {
+            public override void ApplyAppearance()
+            {
+                Texture2D ExampleBG = info.CardBack.texture;
+                ExampleBG.filterMode = FilterMode.Point;
+                base.Card.RenderInfo.baseTextureOverride = ExampleBG;
+            }
+
+            // When this card is chosen via a sequencer, run the following method. This example does nothing, but think *Ijaraq*.
+            public override void OnCardAddedToDeck()
+            {
+
+            }
+        }
+
         public class AppearenceBehaviorInfo
         {
             public string Name;
@@ -26,6 +53,7 @@ namespace JLPlugin.Data
             {
                 string file = files[index];
                 string filename = file.Substring(file.LastIndexOf(Path.DirectorySeparatorChar));
+                string FilePath = Path.GetFullPath(file);
 
                 if (!filename.ToLower().EndsWith("_appearence.jldr2"))
                     continue;
@@ -39,7 +67,58 @@ namespace JLPlugin.Data
                     AppearenceBehaviorInfo AppearenceBehaviors = JSONParser.FromFilePath<AppearenceBehaviorInfo>(file);
                     foreach (AppearenceBehaviorInfo appearencedata in AppearenceBehavior)
                     {
-                        // Grant Logic
+                        // Define an Appearence Info for the Appearence
+                        AppearenceBehaviorInfo Appearence = new AppearenceBehaviorInfo
+                        {
+                            Name = appearencedata.Name,
+                            GUID = appearencedata.GUID,
+                            Layer = appearencedata.Layer,
+                            ImageList = appearencedata.ImageList,
+                            Randomize = appearencedata.Randomize,
+                            RandomizeConditions = appearencedata.RandomizeConditions
+                        };
+                        // Create a List of Sprites for the Appearence
+                        List<Sprite> Imagelist = new List<Sprite> { };
+
+                        // Get the Image Sprite for each Image in the ImageList
+                        foreach (string Image in Appearence.ImageList)
+                        {
+                            string[] SplitImagePath = Image.Split('/');
+                            int PathCount = SplitImagePath.Count();
+                            string ImageName = SplitImagePath.ElementAt(PathCount);
+                            string NewImagePath = Image.Replace('/' + ImageName, "");
+                            if (ImageName.EndsWith(".png"))
+                            {
+                                Sprite MyNewSprite = ImageLoader.GetCustomImage(FilePath + NewImagePath, ImageName);
+                                Imagelist.AppendWith(MyNewSprite);
+                            } else
+                            {
+                                Debug.LogError($"{FilePath + NewImagePath + ImageName} must be of type '.png'.");
+                            }
+                        }
+
+                        // Create Appearence ID
+                        string ID = Appearence.GUID + Appearence.Name;
+
+                        // Define Layer Logic + Randomization Logic
+
+                        if (Appearence.Randomize == true)
+                        {
+                            // When X condition Happens Do X
+                            if (Appearence.RandomizeConditions == "OnRuntime")
+                            {
+                                var RandomizedImageList = Imagelist.Randomize();
+                            }
+                        }
+
+                        if (Appearence.Layer == "CardBacking")
+                        {
+                            new AppearenceBackgroundInfo
+                            {
+                                CardBack = Imagelist[0]
+                            };
+
+                        }
                     }
                 }
                 catch (Exception e)
